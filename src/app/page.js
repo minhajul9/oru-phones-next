b8fb6d6cbfbe613ada46be5b6a105843094c75ba
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-08-15 15:31:21 +0600

    navbar complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,37 @@
 import Image from 'next/image'
 import Navbar from '@/components/Navbar'
+import Link from 'next/link'
+import { FaAlignLeft } from 'react-icons/fa'
 
 export default function Home() {
   return (
     <main className='min-h-screen bg-[#FAFBFF]'>
-      <Navbar></Navbar>
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content ">
+          {/* Page content here */}
+          <Navbar></Navbar>
+          <label htmlFor="my-drawer-2" className="absolute top-10 text-xl ms-3 text-yellow-600 drawer-button lg:hidden">
+            <FaAlignLeft></FaAlignLeft>
+          </label>
+
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 h-full bg-white text-[#1A1558]">
+            {/* Sidebar content here */}
+            <div className='p-2 border m-6 rounded-md shadow-md text-center'>
+              <h1 className='font-Outfit font-bold text-3xl'>Dashboard</h1>
+            </div>
+            <li>
+              <Link href=''>My Profile</Link>
+            </li>
+            <li><Link href=''>My Connection</Link></li>
+          </ul>
+
+        </div>
+      </div>
+
     </main>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,58 +1,36 @@
 import React from 'react';
 import Image from 'next/image'
 import Link from 'next/link';
+import { FaAngleRight, FaBell, FaRegBell } from "react-icons/fa";
 
 const Navbar = () => {
     return (
 
-        <div className="drawer lg:drawer-open">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content ">
-                {/* Page content here */}
-                <div className="navbar bg-white border-b">
-                    <div className="navbar-start">
-                        <div className="dropdown">
-                            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                            </label>
-                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                                <li><a>Item 1</a></li>
-                                <li><a>Item 3</a></li>
-                            </ul>
-                        </div>
-
-                        <a className="btn btn-ghost normal-case text-xl">
-                            <Image src="/Screenshot 2023-08-14 144026.png" alt="" width='100' height='50'></Image>
-                        </a>
-                    </div>
-                    <div className="navbar-center hidden lg:flex">
-
-                    </div>
-                    <div className="navbar-end">
-                        <ul className="menu menu-horizontal px-1">
-                            <li tabIndex={0}>
-                                <details>
-                                    <summary>Parent</summary>
-                                    <ul className="p-2">
-
-                                    </ul>
-                                </details>
-                            </li>
-                        </ul>
-                    </div>
-
-                </div>
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
+        <div className="navbar bg-white relative">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl ms-4">
+                    <Image src='/logo.png' width='100' height='50' alt='Image not found'></Image>
+                </a>
             </div>
-            <div className="drawer-side">
-                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 h-full bg-white text-[#1A1558]">
-                    {/* Sidebar content here */}
-                    <li><Link href=''>My Profile</Link></li>
-                    <li><Link href=''>My Connection</Link></li>
-                </ul>
+            <div className="flex-none">
+                <FaRegBell className='text-[#1A1558] text-3xl mx-4'></FaRegBell>
+                <ul className="menu menu-horizontal px-1">
 
+                    <li>
+                        <details>
+                            <summary className='border text-black me-4 md:me-16'>
+                                <Image src='/avatar.jpg' width='34' height='34' className='rounded' alt='Image not found'></Image>
+                                <div className='md:flex flex-col hidden'>
+                                    <span>Welcome back</span>
+                                    <span className='text-2xl font-bold'>Vishnu Swaroop</span>
+                                </div>
+                            </summary>
+                            <ul className="p-2 bg-base-100">
+                                <li><a>Link 2</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                </ul>
             </div>
         </div>
 
